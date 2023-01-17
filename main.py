ca30794f104a7fdc1863c71a7eb08283f0ae0744
642dave <david.rakovsky@centrum.cz>
2023-01-17 10:08:01 +0100

    vytvoreni okna

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+# Window
+window = Tk()
+window.title("list tasks")
+window.minsize(400, 400)
+window.iconbitmap("icon.ico")
+window.resizable(False, False)
+
+# Main loop
+window.mainloop()
