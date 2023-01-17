7d08b8d6b2097a373f338037a135099e90692ae9
642dave <david.rakovsky@centrum.cz>
2023-01-17 10:18:20 +0100

    definice hlavnich barev a fontu

--- main.py
+++ main.py
@@ -7,5 +7,11 @@ window.minsize(400, 400)
 window.iconbitmap("icon.ico")
 window.resizable(False, False)
 
+# Fonts, colors
+main_font = ("Times New Roman", 12)
+main_color = "#dd7f00"
+button_color = "#e2cff4"
+window.config(bg=main_color)
+
 # Main loop
 window.mainloop()
