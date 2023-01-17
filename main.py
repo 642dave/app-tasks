a462335b616674ce3dcf2b0410da312d95483e70
David Rakovsky <david.rakovsky@centrum.cz>
2023-01-17 13:20:12 +0100

    vytvoreni Scroolbaru a funkce, ktera zajistuje pridani textu do listboxu a smazani po pridani, propojeni scrollbaru a listboxem

--- main.py
+++ main.py
@@ -13,6 +13,13 @@ main_color = "#dd7f00"
 button_color = "#e2cff4"
 window.config(bg=main_color)
 
+
+# Functions
+def add_text():
+    list_box.insert(END, user_input.get())
+    user_input.delete(0, END)
+
+
 # Frames
 input_frame = Frame(window, bg=main_color)
 text_frame = Frame(window, bg=main_color)
@@ -25,7 +32,7 @@ button_frame.pack()
 user_input = Entry(input_frame, width=35, borderwidth=3, font=main_font)
 user_input.grid(row=0, column=0, padx=5, pady=5)
 add_button = Button(input_frame, text="Add", borderwidth=2,
-                    font=main_font, bg=button_color)
+                    font=main_font, bg=button_color, command=add_text)
 add_button.grid(row=0, column=1, padx=5, pady=5, ipadx=10)
 
 # Scrollbar
@@ -34,8 +41,11 @@ text_scrollbar.grid(row=0, column=1, sticky=N+S)
 
 # Text frame - content
 list_box = Listbox(text_frame, height=15, width=45,
-                   borderwidth=3, font=main_font)
+                   borderwidth=3, font=main_font, yscrollcommand=text_scrollbar.set)
 list_box.grid(row=0, column=0)
+
+# Listbox and Scrollbar connection
+text_scrollbar.config(command=list_box.yview)
 
 # Button frame - content
 remove_button = Button(button_frame, text="Remove item",
