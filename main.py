39d12679de93b871fe0ede24f14b4c08eebd08ea
David Rakovsky <david.rakovsky@centrum.cz>
2023-01-19 08:44:11 +0100

    stylizace okna a tlacitek

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ window.minsize(400, 400)
 window.iconbitmap("icon.ico")
 window.resizable(False, False)
 
-# Fonts, colors
+# Main fonts, colors
 main_font = ("Times New Roman", 12)
-main_color = "#dd7f00"
-button_color = "#e2cff4"
+main_color = "#090909"
+button_color = "#4d4d4d"
+fonts_color = "white"
 window.config(bg=main_color)
 
 
@@ -56,18 +57,18 @@ text_frame.pack()
 button_frame.pack()
 
 # Input frame - content
-user_input = Entry(input_frame, width=35, borderwidth=3, font=main_font)
+user_input = Entry(input_frame, width=30, borderwidth=3, font=main_font)
 user_input.grid(row=0, column=0, padx=5, pady=5)
-add_button = Button(input_frame, text="Add", borderwidth=2,
-                    font=main_font, bg=button_color, command=add_text)
-add_button.grid(row=0, column=1, padx=5, pady=5, ipadx=10)
+add_button = Button(input_frame, text="Add", borderwidth=4,
+                    font=main_font, fg=fonts_color, bg=button_color, command=add_text)
+add_button.grid(row=0, column=1, padx=5, pady=5, ipadx=24)
 
 # Scrollbar
 text_scrollbar = Scrollbar(text_frame)
 text_scrollbar.grid(row=0, column=1, sticky=N+S)
 
 # Text frame - content
-list_box = Listbox(text_frame, height=15, width=45,
+list_box = Listbox(text_frame, height=15, width=40,
                    borderwidth=3, font=main_font, yscrollcommand=text_scrollbar.set)
 list_box.grid(row=0, column=0)
 
@@ -76,17 +77,17 @@ text_scrollbar.config(command=list_box.yview)
 
 # Button frame - content
 remove_button = Button(button_frame, text="Remove item",
-                       borderwidth=2, font=main_font, command=remove_text_item)
+                       borderwidth=4, font=main_font, fg=fonts_color, command=remove_text_item, bg=button_color)
 clear_button = Button(button_frame, text="Remove list",
-                      borderwidth=2, font=main_font, command=clear_all_list)
+                      borderwidth=4, font=main_font, fg=fonts_color, command=clear_all_list, bg=button_color)
 save_button = Button(button_frame, text="Save",
-                     borderwidth=2, font=main_font, command=save_tasks)
+                     borderwidth=4, font=main_font, fg=fonts_color, command=save_tasks, bg=button_color)
 quit_button = Button(button_frame, text="Quit",
-                     borderwidth=2, font=main_font, command=window.destroy)
-remove_button.grid(row=0, column=0, padx=2, pady=10)
-clear_button.grid(row=0, column=1, padx=2, pady=10)
-save_button.grid(row=0, column=2, padx=2, pady=10, ipadx=8)
-quit_button.grid(row=0, column=3, padx=2, pady=10, ipadx=8)
+                     borderwidth=4, font=main_font, fg=fonts_color, command=window.destroy, bg=button_color)
+remove_button.grid(row=0, column=0, padx=5, pady=10, ipadx=10)
+clear_button.grid(row=0, column=1, padx=5, pady=10, ipadx=10)
+save_button.grid(row=0, column=2, padx=5, pady=10, ipadx=10)
+quit_button.grid(row=0, column=3, padx=5, pady=10, ipadx=10)
 
 open_tasks()
 
